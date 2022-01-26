ccc8e1d06487fdde61ced1f777e6eb7a55787553
Franz Lutzenberger <franz.lutzenberger@denkwerk.com>
2022-01-26 09:06:57 +0100

    auth module routing added

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: '', component: SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
