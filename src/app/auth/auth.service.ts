2ca2e9a7107348b67378228890d70f9b25a91a8f
Franz Lutzenberger <franz.lutzenberger@denkwerk.com>
2022-01-26 13:30:15 +0100

    refactor username validation to service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface UsernameAvailable {
+  available: true
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  usernameAvailable(username:string) {
+    return this.http
+            .post<UsernameAvailable>('https://api.angular-email.com/auth/username',
+                { username }
+            )
+  }
+}
--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,20 +1,18 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
 import { Observable, of } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 
+import { AuthService } from "../auth.service";
+
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
-    constructor(private http: HttpClient) { }
+    constructor(private auth: AuthService) { }
 
     validate = (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
         const { value } = control
 
-        return this.http
-            .post<any>('https://api.angular-email.com/auth/username',
-                { username: value }
-            )
+        return this.auth.usernameAvailable(value)
             .pipe(
                 map(() => { return null }
                 ),
