053e2b11458c3e76b9d66fc99de96a537eb30fa1
Franz Lutzenberger <franz.lutzenberger@denkwerk.com>
2022-01-31 11:21:34 +0100

    reply working correctly now

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { EmailService } from '../email.service';
+import { Email } from '../models/email';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
+  showModal = false;
 
-  constructor() { }
+  @Input()
+  email!: Email;
 
-  ngOnInit(): void {
+  constructor(private emailService: EmailService) {}
+
+  ngOnChanges(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n----- ${this.email.from} wrote:\n> ${text}`,
+    };
   }
 
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
